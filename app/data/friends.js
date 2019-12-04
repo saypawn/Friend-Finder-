48aa41f18db6f0d3ce300f0480521dedf65b32ec
Jeffery Saephan <jeffery.saephan@gmail.com>
2019-12-04 12:26:11 -0800

    added friends.js and home.html

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+var friendList = [{
+    name: 'Steph Curry',
+    profilePic: 'https://images.search.yahoo.com/images/view;_ylt=Awr9FqzgEehdAV4AdAyJzbkF;_ylu=X3oDMTIzZm4xOHExBHNlYwNzcgRzbGsDaW1nBG9pZANlOTlhZWM4Zjc2NGJmMjY2NDA0NDAxMjk4OTFjNGNmZgRncG9zAzExBGl0A2Jpbmc-?back=https%3A%2F%2Fimages.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dsteph%2Bcurry%26fr%3Dmcafee%26fr2%3Dpiv-web%26tab%3Dorganic%26ri%3D11&w=1000&h=750&imgurl=nationalpostcom.files.wordpress.com%2F2019%2F06%2Fstephen-curry-1.png&rurl=https%3A%2F%2Fnationalpost.com%2Fsports%2Fbasketball%2Fanything-is-possible-the-agony-and-the-ecstasy-of-watching-steph-curry-play-your-team&size=905.4KB&name=%E2%80%98Anything+is+possible%E2%80%99%3A+The+agony+and+the+ecstasy+of+watching+Steph+Curry+play+your+team+...&p=steph+curry&oid=e99aec8f764bf26640440129891c4cff&fr2=piv-web&fr=mcafee&tt=%E2%80%98Anything+is+possible%E2%80%99%3A+The+agony+and+the+ecstasy+of+watching+Steph+Curry+play+your+team+...&b=0&ni=21&no=11&ts=&tab=organic&sigr=140uvrm0k&sigb=13371hh2m&sigi=11v1csang&sigt=130h0lhqj&sign=130h0lhqj&.crumb=my6GrVaqNFS&fr=mcafee&fr2=piv-web',
+    scores: ["2", "5", "3", "4", "1", "5", "5", "3", "2", "5"]
+}, 
+{
+    name: 'Kevin Durant',
+    profilePic: 'https://images.search.yahoo.com/images/view;_ylt=Awr9FqwjEuhd0Q4AoESJzbkF;_ylu=X3oDMTIybDM0dmkzBHNlYwNzcgRzbGsDaW1nBG9pZANiY2NkOTM2MGY5NTU0ODY4YWMxNGJjNTU1Y2NhYmRhMgRncG9zAzcEaXQDYmluZw--?back=https%3A%2F%2Fimages.search.yahoo.com%2Fsearch%2Fimages%3Fp%3Dkevin%2Bdurant%26n%3D60%26ei%3DUTF-8%26fr%3Dmcafee%26fr2%3Dsa-gp-images.search%26tab%3Dorganic%26ri%3D7&w=1200&h=800&imgurl=thenypost.files.wordpress.com%2F2019%2F06%2Fkevin-durant-2.jpg%3Fquality%3D90%26strip%3Dall%26w%3D1200&rurl=https%3A%2F%2Fnypost.com%2F2019%2F06%2F27%2Fkevin-durant-is-on-the-move-literally%2F&size=179.0KB&name=NBA+free+agency%3A+Warriors%26%2339%3B+Kevin+Durant+sells+Malibu+home&p=kevin+durant&oid=bccd9360f9554868ac14bc555ccabda2&fr2=sa-gp-images.search&fr=mcafee&tt=NBA+free+agency%3A+Warriors%26%2339%3B+Kevin+Durant+sells+Malibu+home&b=0&ni=64&no=7&ts=&tab=organic&sigr=124ssql73&sigb=13te18qje&sigi=12k2fvvpb&sigt=11td8gqjk&sign=11td8gqjk&.crumb=my6GrVaqNFS&fr=mcafee&fr2=sa-gp-images.search',
+    scores: ["1", "2", "1", "5", "2", "4", "1", "3", "2", "3"]
+}, 
+{
+    name: 'Damian Lillard',
+    profilePic: 'https://cdn.vox-cdn.com/thumbor/g0E6frWFg6qXlTSUUjDjwDr3hc0=/0x0:3671x2570/1200x800/filters:focal(2010x345:2596x931)/cdn.vox-cdn.com/uploads/chorus_image/image/60436327/usa_today_10799606.0.jpg',
+    scores: ["1", "5", "3", "2", "4", "3", "4", "5", "1", "3"]
+}, 
+{
+    name: 'Kawahi Leonard',
+    profilePic: 'https://www.gannett-cdn.com/presto/2018/11/26/USAT/b831a0b1-88ba-4336-b584-d48912eb0591-2018-11-25_Kawhi_Leonard.jpg?crop=4356,2450,x395,y393&width=3200&height=1680&fit=bounds',
+    scores: ["4", "5", "3", "3", "4", "5", "4", "5", "4", "5"]
+}, 
+{
+    name: 'Lebron James',
+
+    profilePic: 'https://thegrio.com/wp-content/uploads/2018/10/GettyImages-1053412100-e1545001600775.jpg',
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+}]
+
+module.exports = friendList;
